730c7ea2e3dd01851a36f2a4d27be1bf563dd754
Tabitha <tabitha.r.mccracken@gmail.com>
2024-06-03 18:24:14 -0600

    file changes

--- frontend/src/components/VerseCard.jsx
+++ frontend/src/components/VerseCard.jsx
@@ -13,3 +13,54 @@ const VerseCard = ({ verse }) => {
 };
 
 export default VerseCard;
+
+
+
+// // import React from 'react';
+// import React, { useState, useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import axios from 'axios';
+// // import 'dotenv/config'
+// // require('dotenv').config();
+// // const apiKey = process.env.REACT_APP_ESV_API_KEY;
+// // dotenv.config();
+
+// const VersePage = () => {
+//   const location = useLocation();
+//   const { verse } = location.state;
+//   const [verseText, setVerseText] = useState('');
+
+//   useEffect(() => {
+//     const fetchVerseText = async () => {
+//       try {
+//         const { book, chapter, verse: verseNumber } = verse; //??? Maybe takes parts of the verse state?
+//         const response = await axios.get(`https://api.esv.org/v3/passage/text/?q=${book}+${chapter}:${verseNumber}`, {
+//           headers: {
+//             Authorization: `Token ${apiKey}`
+//           }
+//         });
+//         setVerseText(response.data.passages[0]);
+//       } catch (error) {
+//         console.error('Error fetching verse text:', error);
+//       }
+//     };
+
+//     fetchVerseText();
+//   }, [verse]);
+
+
+
+//   if (!verse) {
+//     return <div>Error: No verse data available</div>
+//   }
+
+//   return (
+//     <div className="single-verse-page">
+//       <h2>{verse.book} {verse.chapter}:{verse.verse}</h2>
+//       <p>{verseText}</p>
+//     </div>
+//   );
+// };
+
+// export default VersePage;
+
