ffdfa7af38b493164693a3795e7d50cb1ae0e231
Tabitha <tabitha.r.mccracken@gmail.com>
2024-06-12 14:38:54 -0600

    Updated buttons on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             </li>
             <li>
               <button onClick={() => handleNavigate(`/loggedin/${userId}`)}>
-                Home
+                My Verse Page
               </button>
             </li>
           </>
